acebb64133b5354c4a553fe67f4f4d98fdedcca0
BabdCatha <36963178+BabdCatha@users.noreply.github.com>
2021-04-02 23:53:54 +0200

    Auth is now working
    Email and Google Auth are implemented.

--- app/src/main/java/net/sokato/NewsCheck/MainActivity.java
+++ app/src/main/java/net/sokato/NewsCheck/MainActivity.java
@@ -1,24 +1,42 @@
 package net.sokato.NewsCheck;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import net.sokato.NewsCheck.Fragments.NewsFragment;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
     private NewsFragment newsFragment;
+    private TextView login;
+    private TextView accountType;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +49,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolBar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolBar, R.string.nav_drawer_open, R.string.nav_drawer_close){
+            @Override
+            public void onDrawerSlide(View drawerView, float sideOffset) {  //Set the user icons and stuff here, maybe change the slide to save computing power
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                login = findViewById(R.id.name);
+                if(user == null) {
+                    login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) { //Start the login activity
+                            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+                            // Create and launch sign-in intent
+                            startActivityForResult(
+                                    AuthUI.getInstance()
+                                            .createSignInIntentBuilder()
+                                            .setAvailableProviders(providers)
+                                            .build(),
+                                    7);
+                        }
+                    });
+                }else{
+                    login.setText(R.string.placeHolder);
+                }
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -64,4 +110,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                Toast.makeText(MainActivity.this, R.string.loginFailure, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
 }
