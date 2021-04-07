a807d309d4f24a912db0edab4360a61021fddf24
BabdCatha <36963178+BabdCatha@users.noreply.github.com>
2021-04-07 18:46:42 +0200

    Started to work on the comment system

---
+++ app/src/main/java/net/sokato/NewsCheck/models/Comment.java
@@ -0,0 +1,28 @@
+package net.sokato.NewsCheck.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+    @SerializedName("authorID")
+    @Expose
+    private String authorID;
+
+    @SerializedName("authorName")
+    @Expose
+    private String author;
+
+    @SerializedName("authorStatus")
+    @Expose
+    private String AuthorStatus;
+
+    @SerializedName("commentText")
+    @Expose
+    private String commentText;
+
+    @SerializedName("date")
+    @Expose
+    private String date;
+
+}
